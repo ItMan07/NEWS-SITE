076090c1251967ff5f6d1d1d5bf8ed9d5538ed99
ItMan <kurbanovarslan228@gmail.com>
2023-05-21 21:29:10 +0500

    fix

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+# app.config["SQLALCHEMY_DATABASE_URI"] =
 app.config.from_object('config.Config')
 # images_folder = os.path.join('static', 'images')
 # , static_url_path='/static'
