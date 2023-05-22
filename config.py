4a7dda3171ff78a800c6588c9016fbce3e67f87f
ItMan <kurbanovarslan228@gmail.com>
2023-05-22 20:40:27 +0500

    update

--- config.py
+++ config.py
@@ -5,9 +5,10 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
     SECRET_KEY = os.urandom(16).hex()
-    # WTF_CSRF_SECRET_KEY = os.urandom(16).hex()
-    # WTF_CSRF_SECRET_KEY = os.getenv('WTF_CSRF_SECRET_KEY')
     # SECRET_KEY = os.getenv('SECRET_KEY')
-    # SESSION_COOKIE_DOMAIN = False
+    WTF_CSRF_SECRET_KEY = os.urandom(16).hex()
+    # WTF_CSRF_SECRET_KEY = os.getenv('WTF_CSRF_SECRET_KEY')
+
+    SESSION_COOKIE_DOMAIN = False
     # SESSION_COOKIE_DOMAIN = 'itman7144.pythonanywhere.com'
-    # SERVER_NAME = 'itman7144.pythonanywhere.com'
+    SERVER_NAME = 'itman7144.pythonanywhere.com'
