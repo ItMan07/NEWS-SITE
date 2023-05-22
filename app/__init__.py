3f5f5b59b2ab99fc7271e005b2c71cdb8ce21d03
ItMan <kurbanovarslan228@gmail.com>
2023-05-22 20:33:29 +0500

    update

--- app/__init__.py
+++ app/__init__.py
@@ -4,15 +4,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
-# csrf = CSRFProtect(app)
+csrf = CSRFProtect(app)
 app.config.from_object('config.Config')
-# app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///flask-site-db.sqlite3'
-# app.config["SECRET_KEY"] = 'HKJFghedfkh784yuHKJFh430789/L:F34kl;3rP&y4809rywahrf'
-# app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-
-# images_folder = os.path.join('static', 'images')
-# , static_url_path='/static'
-# app.config['UPLOAD_FOLDER'] = images_folder
 
 db = SQLAlchemy(app)
 from .views import *
