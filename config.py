47dacd3f63c89c668ddc6929e496136c81fa127f
ItMan <kurbanovarslan228@gmail.com>
2023-05-22 18:38:06 +0500

    add WTF_CSRF_SECRET_KEY

--- config.py
+++ config.py
@@ -4,4 +4,5 @@ import os
 class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
     SECRET_KEY = os.getenv('SECRET_KEY')
+    WTF_CSRF_SECRET_KEY = os.getenv('WTF_CSRF_SECRET_KEY')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
