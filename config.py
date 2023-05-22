ea5b9b68bd25b2239b68f10949369a2020c16a14
ItMan <kurbanovarslan228@gmail.com>
2023-05-22 19:41:35 +0500

    fix

--- config.py
+++ config.py
@@ -2,10 +2,10 @@ import os
 
 
 class Config:
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
     SECRET_KEY = os.getenv('SECRET_KEY')
-    WTF_CSRF_SECRET_KEY = os.getenv('WTF_CSRF_SECRET_KEY')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # WTF_CSRF_SECRET_KEY = os.getenv('WTF_CSRF_SECRET_KEY')
     # SESSION_COOKIE_DOMAIN = False
-    SESSION_COOKIE_DOMAIN = 'itman7144.pythonanywhere.com'
+    # SESSION_COOKIE_DOMAIN = 'itman7144.pythonanywhere.com'
     # SERVER_NAME = 'itman7144.pythonanywhere.com'
