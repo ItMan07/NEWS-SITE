679dd3f35b9983a363b72d100f848bed4691d326
ItMan <kurbanovarslan228@gmail.com>
2023-05-22 21:37:23 +0500

    update

--- config.py
+++ config.py
@@ -10,8 +10,10 @@ class Config:
     # WTF_CSRF_SECRET_KEY = os.getenv('WTF_CSRF_SECRET_KEY')
     WTF_CSRF_CHECK_DEFAULT = False
 
-    SESSION_COOKIE_DOMAIN = False
+    # SESSION_COOKIE_DOMAIN = False
     # SESSION_COOKIE_DOMAIN = 'itman7144.pythonanywhere.com'
     # SERVER_NAME = 'itman7144.pythonanywhere.com'
+    SESSION_COOKIE_DOMAIN = None
+    SERVER_NAME = None
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
