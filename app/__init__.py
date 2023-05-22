fd5f7fcf070b374cd857b7546c6447ad9dbfe2fc
ItMan <kurbanovarslan228@gmail.com>
2023-05-22 20:02:55 +0500

    disable CSRFProtect

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
-csrf = CSRFProtect(app)
+# csrf = CSRFProtect(app)
 app.config.from_object('config.Config')
 # app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///flask-site-db.sqlite3'
 # app.config["SECRET_KEY"] = 'HKJFghedfkh784yuHKJFh430789/L:F34kl;3rP&y4809rywahrf'
