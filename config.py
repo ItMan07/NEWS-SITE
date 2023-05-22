46d0a8573b749c98a666aabebea3c7a454da8682
ItMan <kurbanovarslan228@gmail.com>
2023-05-22 21:43:12 +0500

    update

--- config.py
+++ config.py
@@ -7,12 +7,12 @@ class Config:
     # SECRET_KEY = os.urandom(16).hex()
     SECRET_KEY = os.getenv('SECRET_KEY')
     # WTF_CSRF_SECRET_KEY = os.urandom(16).hex()
-    # WTF_CSRF_SECRET_KEY = os.getenv('WTF_CSRF_SECRET_KEY')
+    WTF_CSRF_SECRET_KEY = os.getenv('WTF_CSRF_SECRET_KEY')
     # WTF_CSRF_CHECK_DEFAULT = False
 
-    # SESSION_COOKIE_DOMAIN = False
+    SESSION_COOKIE_DOMAIN = False
     # SESSION_COOKIE_DOMAIN = 'itman7144.pythonanywhere.com'
-    # SERVER_NAME = 'itman7144.pythonanywhere.com'
+    SERVER_NAME = 'itman7144.pythonanywhere.com'
     # SESSION_COOKIE_DOMAIN = None
     # SERVER_NAME = None
 
