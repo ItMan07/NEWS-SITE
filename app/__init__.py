67a589659b0173fdce70497b8471e129a0a33c8e
ItMan <kurbanovarslan228@gmail.com>
2023-05-22 16:08:27 +0500

    fix csrf

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,15 @@
 from flask import Flask
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
+from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
+csrf = CSRFProtect(app)
+app.config.from_object('config.Config')
 # app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///flask-site-db.sqlite3'
 # app.config["SECRET_KEY"] = 'HKJFghedfkh784yuHKJFh430789/L:F34kl;3rP&y4809rywahrf'
 # app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.config.from_object('config.Config')
+
 # images_folder = os.path.join('static', 'images')
 # , static_url_path='/static'
 # app.config['UPLOAD_FOLDER'] = images_folder
@@ -17,7 +20,6 @@ from .models import *
 
 app.app_context().push()
 db.create_all()
-
 manager = LoginManager(app)
 
 
