76b83d5b28b034b389fa97e1fac5db9426d2a0b6
ItMan <kurbanovarslan228@gmail.com>
2023-05-22 21:52:23 +0500

    update

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ class Config:
     # WTF_CSRF_CHECK_DEFAULT = True
 
     # SESSION_COOKIE_DOMAIN = False
-    # SESSION_COOKIE_DOMAIN = 'itman7144.pythonanywhere.com'
+    SESSION_COOKIE_DOMAIN = 'itman7144.pythonanywhere.com'
     # SERVER_NAME = 'itman7144.pythonanywhere.com'
     # SESSION_COOKIE_DOMAIN = None
     # SERVER_NAME = None
