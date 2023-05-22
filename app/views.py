4a749fef9544cd9f66a47d734f61eda20c5e3d0e
ItMan <kurbanovarslan228@gmail.com>
2023-05-22 15:32:38 +0500

    fix config, current_user.is_authenticated, csrf token. Update secret key. Delete .env. Add .gitignore

--- app/views.py
+++ app/views.py
@@ -37,12 +37,11 @@ def about():
 
 @app.route('/news/<int:news_id>')
 def news_detail(news_id):
-    try:
-        user_id = User.query.get(current_user.id)
-        is_admin = True if user_id.admin else False
-    except:
-        is_admin = False
-    # is_author = True if News.query.get(news_id).author_id == user_id else False
+    is_admin = False
+    if current_user.is_authenticated:
+        user = User.query.get(current_user.id)
+        is_admin = True if user.admin else False
+        # is_author = True if News.query.get(news_id).author_id == user_id else False
 
     data = {
         'news': News.query.get(news_id),
