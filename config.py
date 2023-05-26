8afd9ea3dd4454a308ca487267f800c3094e1a69
ItMan <kurbanovarslan228@gmail.com>
2023-05-26 17:41:18 +0500

    update

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
 
     SECRET_KEY = os.urandom(16).hex()
-    # CSRF_ENABLED = True
+    CSRF_ENABLED = True
     # SECRET_KEY = os.getenv('SECRET_KEY')
     # WTF_CSRF_TIME_LIMIT = 5
     # WTF_CSRF_SECRET_KEY = os.urandom(16).hex()
@@ -13,8 +13,8 @@ class Config:
     # WTF_CSRF_CHECK_DEFAULT = True
 
     # SESSION_COOKIE_DOMAIN = False
-    # SESSION_COOKIE_DOMAIN = 'itman7144.pythonanywhere.com'
-    # SERVER_NAME = 'itman7144.pythonanywhere.com'
+    SESSION_COOKIE_DOMAIN = 'itman7144.pythonanywhere.com'
+    SERVER_NAME = 'itman7144.pythonanywhere.com'
 
     # SESSION_COOKIE_DOMAIN = None
     # SERVER_NAME = None
