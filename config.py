e73d3e79b9c952c147b5edb6fca52b203728ef02
ItMan <kurbanovarslan228@gmail.com>
2023-05-26 17:39:55 +0500

    update

--- config.py
+++ config.py
@@ -4,10 +4,10 @@ import os
 class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
 
-    # SECRET_KEY = os.urandom(16).hex()
-    CSRF_ENABLED = True
-    SECRET_KEY = os.getenv('SECRET_KEY')
-    WTF_CSRF_TIME_LIMIT = 5
+    SECRET_KEY = os.urandom(16).hex()
+    # CSRF_ENABLED = True
+    # SECRET_KEY = os.getenv('SECRET_KEY')
+    # WTF_CSRF_TIME_LIMIT = 5
     # WTF_CSRF_SECRET_KEY = os.urandom(16).hex()
     # WTF_CSRF_SECRET_KEY = os.getenv('WTF_CSRF_SECRET_KEY')
     # WTF_CSRF_CHECK_DEFAULT = True
